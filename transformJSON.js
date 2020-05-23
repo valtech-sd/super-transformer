27ffbee7c89ab63b28ba93845e2ba022da826980
Eric Soto <Eric.Soto@valtech.com>
2020-05-22 21:08:13 -0400

    Adds support for Handlebars Custom Helpers (straggler files) - Adds loadHandlebarsHelpers to TemplateHelpers, providing a simple way to load any properly formed JS file containing Handlebars registrer statements. - Adds a demo template to properly show/test the feature. - Adds an example Handlebars Helpers javascript demonstrating the syntax. - Adds a test for the new loadHandlebarshelpers method. - Increments package.json for this update. - Enhanced README.md covers the new feature and provides examples. - Adds support for the new feature in the 4 command line scripts.

--- transformJSON.js
+++ transformJSON.js
@@ -11,7 +11,10 @@ const path = require('path');
 
 // Set aside a variable to hold our inputObject once we parse it from JSON
 // These will be set by our programSetup
-let inputObject, absoluteTemplatePath, inputDataLocal;
+let inputObject,
+  absoluteTemplatePath,
+  inputDataLocal,
+  absoluteHandlebarsHelperPath;
 
 // Parse out command line arguments
 programSetup(program);
@@ -58,6 +61,10 @@ function programSetup(program) {
     .option(
       "-r, --replacepattern <'a replacement pattern'>",
       "If you passed in -m, pass a regex replacement pattern in this argument. This is ignored if you don't also pass -m."
+    )
+    .option(
+      "-x --extendHandlebars </path/to/your/handlebars-extension.js>'",
+      'If you provide a path to a javascript file'
     )
     .parse(process.argv);
 
@@ -110,7 +117,7 @@ function programSetup(program) {
 
   try {
     // Make the template file absolute
-    absoluteTemplatePath = path.normalize(program.template);
+    absoluteTemplatePath = path.resolve(program.template);
   } catch (e) {
     program.outputHelp();
     console.error(
@@ -126,5 +133,39 @@ function programSetup(program) {
       `\n>>> ERROR: the template file "${absoluteTemplatePath}" was not found!`
     );
     process.exit(-31);
+  }
+
+  // Sanity for the Handlebars helper file, if passed
+  if (program.extendHandlebars) {
+    try {
+      // Make the path absolute
+      absoluteHandlebarsHelperPath = path.resolve(program.extendHandlebars);
+    } catch (e) {
+      program.outputHelp();
+      console.error(
+        `\n>>> ERROR: Could not parse the path of your handlebars helper javascript file from the supplied value of "${program.extendHandlebars}".`
+      );
+      process.exit(-40);
+    }
+
+    // Ensure the file exists
+    if (!fs.existsSync(absoluteHandlebarsHelperPath)) {
+      program.outputHelp();
+      console.error(
+        `\n>>> ERROR: the Handlebars helper javascript file "${absoluteHandlebarsHelperPath}" was not found!`
+      );
+      process.exit(-41);
+    }
+
+    // ASSERT: We have an absolute path & File exists, so we load it
+    try {
+      TemplateHelper.loadHandlebarsHelpers(absoluteHandlebarsHelperPath);
+    } catch (e) {
+      console.error(
+        `\n>>> ERROR: Could register the helpers in your handlebars helper javascript file from the supplied value of "${absoluteHandlebarsHelperPath}".`
+      );
+      console.error(`>>> INNER ERROR: ${e.message}`);
+      process.exit(-40);
+    }
   }
 }
